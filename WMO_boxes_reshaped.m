3ba10ec86d6d6945e0209b178d4c3c7b9cb9afb2
gnotarstefano <59864458+gnotarstefano@users.noreply.github.com>
2021-09-24 11:37:29 +0200

    Add files via upload

--- WMO_boxes_reshaped.m
+++ WMO_boxes_reshaped.m
@@ -1,7 +1,7 @@
 function []=WMO_boxes_reshaped(path_1,EL,NL,WMOB)
 
 % DESCRIPTION:
-%    CTD profiles are grouped per sub-basins of the Mediterranean and Black Seas. 
+%    CTD profiles are grouped per sub-basins. 
 %    WMO boxes used in OWC are re-shaped according to the dimension of the sub-basins. 
 %    WMO boxes dimensions could be too large for OWC application in marginal seas.
 %    The WMO boxes reshaping is useful to avoid selecting historical data for 
